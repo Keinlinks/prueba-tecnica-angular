952990b72fd96a06335a4194a0309cbbb3edcba2
KevinClift <kevin.clift.b@gmail.com>
2025-12-30 16:38:36 -0300

    added early return in case production environment in logger service

--- src/app/core/services/logger.service.ts
+++ src/app/core/services/logger.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from "@angular/core";
+import { environment } from "@src/environments/environment";
 
 @Injectable({providedIn: 'root'})
 export class LoggerService {
   constructor() {}
 
   log(message: string): void {
+    if (environment.production) return;
+
     console.log(`${new Date().toISOString()}: ${message}`);
   }
   error(message: string): void {
+    if (environment.production) return;
+
     console.error(`${new Date().toISOString()}: ${message}`);
   }
 }
